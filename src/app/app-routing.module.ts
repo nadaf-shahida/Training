fc63d515febd00fb559f97e72378bec5dcb4a1d6
shahida-vervotech <shahida@techspian.com>
2021-07-22 18:54:49 +0530

    assignment first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginPageComponent},
+  {path:'signup',component:SignupPageComponent},
+  {path:'navbar',component:NavbarComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
